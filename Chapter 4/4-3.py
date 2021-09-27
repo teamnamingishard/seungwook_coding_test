8daf95b3c5725520d25936a165b21ff9e94b87f6
SeungWookHan <hswook12@me.com>
2021-09-27 21:02:32 +0900

    Add 4-3

---
+++ Chapter 4/4-3.py
@@ -0,0 +1,16 @@
+input = input()
+row = int(input[1])
+col = int(ord(input[0])) - int(ord("a")) + 1
+
+steps = [(-2, -1), (-1, -2), (2, -1), (-1, 2), (-2, 1), (1, -2), (2, 1), (1, 2)]
+
+result = 0
+
+for step in steps:
+    next_row = row + step[0]
+    next_col = col + step[1]
+
+    if next_row >= 1 and next_row <= 8 and next_col >= 1 and next_col <= 8:
+        result += 1
+
+print(result)
