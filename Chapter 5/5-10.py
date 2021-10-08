263abfdbd0f49acaf61f5400fbb42cdf3ecd398a
SeungWookHan <hswook12@me.com>
2021-10-09 04:48:08 +0900

    Add 5-10

---
+++ Chapter 5/5-10.py
@@ -0,0 +1,35 @@
+### 음료수 얼려먹기
+
+n, m = map(int, input().split())
+
+# 2차원 리스트 맵 정보 입력 받기
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# DFS로 특정한 노드 방문한 뒤 연결된 모든 노드 방문
+def dfs(x, y):
+    # 범위 제한
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        # 상, 하, 좌, 우의 위치 모두 재귀적 호출
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
+    return False
+
+
+# 모든 노드(위치)에 대하여 음료수 채우기
+result = 0
+for i in range(n):
+    for j in range(m):
+        # 현재 위치에서 DFS 수행
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
