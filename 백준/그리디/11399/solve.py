8bb7963fa53ec58f13faf915eafc8f4559c80820
SeungWookHan <hswook12@me.com>
2021-12-17 22:16:57 +0900

    Add solve

--- 백준/그리디/11399/solve.py
+++ 백준/그리디/11399/solve.py
@@ -3,7 +3,10 @@ from typing import List
 
 
 def find_minimum_time(N: int, P: List):
-  pass
+  P.sort()
+  for i in range(1, N):
+    P[i] += P[i-1]
+  return sum(P)
 
 if __name__ == "__main__":
   N = int(input())
